c8a67245462b92f6947d9b9a174fbc2959f70227
shikwi <crpandas@163.com>
2025-10-22 00:34:20 +0800

    DataBinConvert

---
+++ DataBinConvert/GameDataController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinConvert
+{
+    public class GameDataController
+    {
+        public Dictionary<object, byte[]> LoadCommonBinData(byte[] data)
+        {
+            var records = new Dictionary<object, byte[]>();
+
+            using (var ms = new MemoryStream(data))
+            using (var reader = new BinaryReader(ms, Encoding.UTF8))
+            {
+                reader.ReadInt32();  //Magic
+                short strLen = reader.ReadInt16();
+                reader.ReadBytes(strLen);   //Sig String
+
+                byte keyType = reader.ReadByte();
+                byte valueType = reader.ReadByte();
+                int recordCount = reader.ReadInt32();
+
+                if (keyType == 1)
+                {
+                    if (valueType == 1)  //key: int, value: byte[]
+                    {
+                        for (int i = 0; i < recordCount; i++)
+                        {
+                            records.Add(reader.ReadInt32(), reader.ReadBytes(reader.ReadInt16()));
+                        }
+                    }
+                    else if (valueType == 2)  //key: long, value: byte[]
+                    {
+                        for (int i = 0; i < recordCount; i++)
+                        {
+                            records.Add(reader.ReadInt64(), reader.ReadBytes(reader.ReadInt16()));
+                        }
+                    }
+                    else  //Key: string, value: byte[]
+                    {
+                        for (int i = 0; i < recordCount; i++)
+                        {
+                            records.Add(Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadInt16())), reader.ReadBytes(reader.ReadInt16()));
+                        }
+                    }
+                }
+                else
+                {
+                    for(int i = 0; i< recordCount; i++)
+                    {
+                        records.Add(i + 1, reader.ReadBytes(reader.ReadInt16()));
+                    }
+                }
+
+            }
+
+            return records;
+        }
+
+    }
+}
---
+++ DataBinConvert/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using DataBinConvert;
+using Google.Protobuf;
+using Newtonsoft.Json;
+using StellaSoraParser.Proto;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string inputFile = "86_CB413F24986EF082.bin";
+        string ProtoTypeName = "StellaSoraParser.Proto.DatingCharacterEvent";
+
+        string outputFile = ProtoTypeName + ".json";
+        try
+        {
+            ConvertSingleBinToJson(inputFile, outputFile, ProtoTypeName);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+        }
+
+    }
+
+    public static void ConvertSingleBinToJson(string infile, string outfile, string typeName)
+    {
+        const int Magic = 0x00039354;
+        if(!Path.Exists(infile))
+        {
+            throw new FileNotFoundException($"Cannot Find {infile}");
+        }
+
+        byte[] filebytes = File.ReadAllBytes(infile);
+
+        if(filebytes.Length < 4 || BitConverter.ToInt32(filebytes, 0) != Magic)
+        {
+            Console.WriteLine("Plain Text, Skip Convert");
+
+            try
+            {
+                object jsonobj = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(filebytes));
+                string formatJson = JsonConvert.SerializeObject(jsonobj, Formatting.Indented);
+                File.WriteAllText(outfile, formatJson);
+            }
+            catch
+            {
+                File.WriteAllBytes(outfile, filebytes);
+                return;
+            }
+        }
+
+        var converter = new GameDataController();
+        Dictionary<object, byte[]> rawRecords = converter.LoadCommonBinData(filebytes);
+
+        Type ProtoType = Type.GetType(typeName, throwOnError: true);
+        PropertyInfo parserProperty = ProtoType.GetProperty("Parser", BindingFlags.Public | BindingFlags.Static)!;
+        object parserInstance = parserProperty.GetValue(null)!;
+        MethodInfo parseMethod = parserInstance.GetType().GetMethod("ParseFrom", new[] { typeof(byte[]) })!;
+
+        var JsonOutput = new Dictionary<object, object>();
+        foreach(var record in rawRecords)
+        {
+            object msg = parseMethod.Invoke(parserInstance, new object[] { record.Value })!;
+            JsonOutput.Add(record.Key, msg);
+        }
+
+        var jsonSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            Converters = new List<Newtonsoft.Json.JsonConverter> { new Newtonsoft.Json.Converters.StringEnumConverter() }
+        };
+        string JsonOutputStr = JsonConvert.SerializeObject(JsonOutput, jsonSettings);
+
+        File.WriteAllText(outfile, JsonOutputStr);
+        Console.WriteLine($"Successfully Converted {infile} to {outfile}");
+    }
+
+}
